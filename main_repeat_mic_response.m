810f1f82096f59176c4333d08ab3e2e163d4691a
DongWook Lee <dongwook.lee@kaist.ac.kr>
2018-01-17 20:33:54 +0900

    ignore result dir. writing code added

--- main_repeat_mic_response.m
+++ main_repeat_mic_response.m
@@ -27,19 +27,21 @@ amplify          = 10.^(freqResp_ts./10)';
 
 %%
 % keyboard;
-NTimes = [1,5,10];
+NTimes = [1,5,10,100];
 N = length(NTimes)+1; M = 1;
 disp_amp = getSingleSideMagnitudeSpectrum(amplify,fs,1);
 figure(5); subplot(N,M,1); semilogx(f_vec,disp_amp); title(['Amplifying spectrum of ' fname]); 
 hold on; semilogx(f_vec,ones(1,length(disp_amp)),'--r');  hold off;
 figure(6); subplot(N,M,1); plot(t_vec,v); title('Original source, v(t)');
 for iN=1:length(NTimes)
-    yf_amp = vf.*amplify.^(NTimes(iN));
+    nRep = NTimes(iN);
+    yf_amp = vf.*amplify.^(nRep);
     P1_ampN = real(ifft(yf_amp));
-%     P1_ampN = P1_ampN./max(P1_ampN(:));
+
     sound(P1_ampN,fs);
-    figure(5); subplot(N,M,iN+1); semilogx(f_vec, getSingleSideMagnitudeSpectrum(yf_amp,fs));  title(['amplitude spectrum (after mic ' num2str(iN) ' times)']); ylabel('|Y(f)|'); axis([0,22300,0,inf]);
-    figure(6); subplot(N,M,iN+1); plot(t_vec,P1_ampN); title(['y_{' num2str(iN) '}(t) (after mic ' num2str(iN) ' times)']);
-    pause(l_y);
+    figure(5); subplot(N,M,iN+1); semilogx(f_vec, getSingleSideMagnitudeSpectrum(yf_amp,fs));  title(['amplitude spectrum (after mic ' num2str(nRep) ' times)']); ylabel('|Y(f)|'); axis([0,22300,0,inf]);
+    figure(6); subplot(N,M,iN+1); plot(t_vec,P1_ampN); title(['y_{' num2str(iN) '}(t) (after mic ' num2str(nRep) ' times)']);
+%     pause(l_y);
+%     audiowrite(['result\mic(' fname ')_' num2str(nRep) 'times.wav'],P1_ampN,fs); keyboard;
 end
 figure(5); xlabel('f(Hz)'); figure(6); xlabel('time (sec)');
